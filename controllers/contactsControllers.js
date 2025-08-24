ca779c26ea19494f5ceaa3b1aa0d2237a0ee882e
Oleksandr Knyaz <lichknyaz@gmail.com>
2025-08-24 16:33:15 +0200

    ver 1.0

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,46 @@
-import contactsService from "../services/contactsServices.js";
+import {
+    getContactById,
+    listContacts,
+    removeContact,
+    addContact,
+    updateContactById
+} from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+        const contacts = await listContacts();
+        return res.json(contacts);
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+    const { id } = req.params;
+    const contact = await getContactById(id);
+    if (contact === null) {
+        return res.status(404).json({"message": "Not found"})
+    }
+    return res.json(contact);
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+    const {id} = req.params;
+    const contact = await removeContact(id);
+    if (contact === null) {
+        return res.status(404).json({"message": "Not found"})
+    }
+    return res.json(contact);
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res) => {
+    const { name, email, phone } = req.body;
+    const newContact = await addContact(name, email, phone);
+    return res.status(201).json(newContact);
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res) => {
+    const {id} = req.params;
+    const contact = await getContactById(id)
+    if (contact === null) {
+        return res.status(404).json({"message": "Not found"})
+    }
+    const updatedContact = await updateContactById(id,req.body);
+    return res.status(201).json(updatedContact);
+};
